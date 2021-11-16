1ee6fbf0495cf0637043b76271393e43aa48ab95
Netanel <netanellevine123@gmail.com>
2021-11-16 23:44:18 +0200

    Changes by yanir

--- Elevator.py
+++ Elevator.py
@@ -1,8 +1,7 @@
-import math
 
-import numpy as np
 
 from Calls import Calls
+from Call import Call
 
 
 class Elevator:
@@ -21,13 +20,17 @@ class Elevator:
         self.state = 0  # 0 waiting -1 down 1 up
         self.up = []
         self.down = []
-        self.calls = []# self.calls_of_elev = Calls()
+        self.calls = []  # self.calls_of_elev = Calls()
         self.sim = []
+        self.flag = 0
+        self.num_of_floors = 0
+        self.fixed_dist = 0
 
     def get_distance(self, src, dest):
         dif = abs(src - dest)
         dist = self.openTime + self.stopTime + self.closeTime + self.startTime + (dif / self.speed)
         return dist
+
     # def add_to_route(self, elev):
     #     elev_calls = self.calls_of_elev.calls
     #     if len(self.calls_of_elev.calls) == 1:
@@ -42,3 +45,118 @@ class Elevator:
 
     def __str__(self):
         return f"elev ID: {self.id}  start time: {self.startTime}  stop time: {self.stopTime}  open time: {self.openTime} +  close time: {self.closeTime}  max floor: {self.maxFloor}  min floor: {self.minFloor}  speed: {self.speed}  pos: {self.pos} "
+
+    def check_done_calls(self):
+        for i in self.calls:
+            if i.status!=3:
+                return False
+        return True
+    def curr_pos(self):
+        ind = 0
+        li = self.timeBetweenCalls()
+        for i in li:
+            if len(self.sim)>0 and self.fixed_dist > 0:
+                dif = abs(self.pos - self.sim[0])
+                dist = self.openTime + self.stopTime + self.closeTime + self.startTime + (dif / self.speed)
+                time = self.dister(dist,i,dif)
+                while time == -1:
+                    self.pos = self.sim[0]
+                    del self.sim[0]
+                    if len(self.sim) == 0:
+                        if self.check_done_calls():
+                            return self.pos
+                    self.dister(dist, i, dif)
+                if time == 0:
+                    self.pos = self.sim[0]
+                    del self.sim[0]
+                if self.flag > 1:
+                    self.pos = self.pos+self.num_of_floors
+                    self.flag = 1
+                    self.fixed_dist = time
+                if self.flag > 2:
+                    self.pos = self.sim[0]
+                    del self.sim[0]
+                    self.fixed_dist = time
+            elif len(self.sim)>0:
+                dif = abs(self.pos - self.sim[0])
+                #if flag == 1
+    def dister(self,dist,time,dif):
+        if dist < time:
+            return -1
+        elif dist == time:
+            return 0
+        if time >= self.closeTime:
+            dist = dist - self.closeTime
+            time = time - self.closeTime
+        else:
+            self.flag == 0
+            return dist - time
+        if time >= self.startTime:
+            dist = dist - self.startTime
+            time = time - self.startTime
+        else:
+            self.flag == 1
+            return dist - time
+        if time >= dif/self.speed:
+            dist = dist - dif/self.speed
+            time = time - dif/self.speed
+        else:
+            self.flag == 2
+            ind = 0.0
+            count = 0
+            while ind < dif:
+                if ind/self.speed < time:
+                    if self.state == 1:
+                        count+=1
+                    if self.state == -1:
+                        count+=-1
+                    ind+=1
+            self.num_of_floors = count
+            return dist - time
+        if time >= self.stopTime:
+            self.flag == 3
+            dist = dist - self.stopTime
+            time = time - self.stopTime
+        self.flag == 4
+        return dist - time
+
+    def add_call_to_route_0(self, call):
+        self.sim.append(call.src)
+        call.status = 2
+        if call.src > self.pos:
+            self.state == 1
+        elif call.src < self.pos:
+            self.state == -1
+        else:
+            self.sim.remove(call.src)
+            if call.dest > self.pos:
+                self.state == 1
+                self.sim.append(call.dest)
+                call.status = 3
+            elif call.dest < self.pos:
+                self.state == -1
+                self.sim.append(call.dest)
+                call.status = 3
+
+    def add_call_to_route_1(self, call):
+        if call.src >= self.pos:
+            self.sim.append(call.src)
+            call.status = 2
+        if call.dest >= call.src >= self.pos:
+            self.sim.append(call.dest)
+            call.status = 3
+
+    def add_call_to_route_2(self, call):
+        if call.src <= self.pos:
+            self.sim.append(call.src)
+            call.status = 2
+        if call.dest <= call.src <= self.pos:
+            self.sim.append(call.dest)
+            call.status = 3
+
+    def timeBetweenCalls(self):
+        if len(self.calls) > 0:
+            li = [float(self.calls[0].time)]
+            for i in range(1, len(self.calls)):
+                li.append(float(self.calls[i].time) - float(self.calls[i - 1].time))
+            return li
