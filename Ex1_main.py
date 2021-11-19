4244410ad7fc2f68c5ab7cf7eadb943aef706378
yanir75 <66729230+yanir75@users.noreply.github.com>
2021-11-19 15:12:23 +0200

    Update Ex1_main.py
    updated main

--- Ex1_main.py
+++ Ex1_main.py
@@ -8,16 +8,17 @@ def main():
         file_name = sys.argv[3]
         Reader.read_calculate_write(building_file, csv_file, file_name)
     except:
+        output = sys.argv[2]
         buildings = ["./building_files/B1.json", "./building_files/B2.json", "./building_files/B3.json", "./building_files/B4.json", "./building_files/B5.json"]
         calls_cases = ["./call_files/calls_a.csv", "./call_files/calls_b.csv", "./call_files/calls_c.csv", "./call_files/calls_d.csv"]
-        Reader.read_calculate_write(buildings[1], calls_cases[0], "_B1_a")
-        Reader.read_calculate_write(buildings[0], calls_cases[0], "_B2_a")
+        Reader.read_calculate_write(buildings[1], calls_cases[0], f'{output}_B1_a')
+        Reader.read_calculate_write(buildings[0], calls_cases[0], f'{output}_B2_a')
         ind = 3
         for i in range(2, 5):
             ch = chr(97)
             ind1 = 1
             for j in calls_cases:
-                st = "_B" + str(ind) + "_" + ch
+                st = output+"_B" + str(ind) + "_" + ch
                 Reader.read_calculate_write(buildings[i], j, st)
                 ch = chr(97 + ind1)
                 ind1 += 1
